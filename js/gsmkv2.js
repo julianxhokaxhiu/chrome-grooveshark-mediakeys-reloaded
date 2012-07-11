a04c8f48be0e6d912154d5b5a50413bba6bbd304
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2012-07-11 22:08:59 +0200

    Latest revision available on Chrome Web Store

--- js/gsmkv2.js
+++ js/gsmkv2.js
@@ -1,18 +1,18 @@
 var mkListener = function(e){
-	chrome.extension.sendRequest({ctrlKey:e.ctrlKey,altKey:e.altKey,shiftKey:e.shiftKey,keyCode:e.keyCode},function(r){
+	chrome.extension.sendMessage({ctrlKey:e.ctrlKey,altKey:e.altKey,shiftKey:e.shiftKey,keyCode:e.keyCode},function(r){
 		switch(r.keyCode){
 			case 176:
 				//NEXT_MK
-				chrome.extension.sendRequest({type:"tabid"},function(j){
+				chrome.extension.sendMessage({type:"tabid"},function(j){
 					if(j.isgs)$('#player_next').click();
-					else chrome.extension.sendRequest({cmd:"next"});
+					else chrome.extension.sendMessage({cmd:"next"});
 				});
 				break;
 			case 177:
 				//PREV_MK
-				chrome.extension.sendRequest({type:"tabid"},function(j){
+				chrome.extension.sendMessage({type:"tabid"},function(j){
 					if(j.isgs)$('#player_previous').click();
-					else chrome.extension.sendRequest({cmd:"prev"});
+					else chrome.extension.sendMessage({cmd:"prev"});
 				});
 				break;
 			case 178:
@@ -20,16 +20,16 @@ var mkListener = function(e){
 				break;
 			case 179:
 				//PLAY_MK
-				chrome.extension.sendRequest({type:"tabid"},function(j){
+				chrome.extension.sendMessage({type:"tabid"},function(j){
 					if(j.isgs)$('#player_play_pause').click();
-					else chrome.extension.sendRequest({cmd:"play_pause"});
+					else chrome.extension.sendMessage({cmd:"play_pause"});
 				});
 				break;
 		}
 	})
 }
 $(window).keyup(function(e){mkListener(e)});
-chrome.extension.onRequest.addListener(function(request,sender,sendResponse){
+chrome.extension.onMessage.addListener(function(request,sender,sendResponse){
 	switch(request){
 		case 'next':$('#player_next').click();
 		break;
