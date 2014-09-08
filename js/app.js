4951f755b12c07adeaf53626dc364e570314946c
Georgios Tzourmpakis <kiougar@gmail.com>
2014-09-08 16:59:22 +0300

    Update app.js
    Removed the console.log

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
     var click_event = new Event('click');
-    console.log('Command:', request.action);
+    
     switch(request.action){
         case 'NEXT-MK':
             //NEXT_MK
